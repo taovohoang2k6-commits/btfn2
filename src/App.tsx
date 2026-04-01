d350fd604ca24c1663d79fbe1200b84b2f3b0deb
Hoang <taovohoang2k6@gmail.com>
2026-04-01 15:46:16 +0700

    lab8

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,106 @@
-// import { Toaster } from "react-hot-toast";
-// import { Link,Routes, Route } from "react-router-dom";
-// import { Layout } from "antd";
-// import Dashboard from "./pages/Dashboard";
-// import Login from "./pages/Login";
-// import Uers from "./pages/Uers";
-// import Product from "./pages/Product";
-// import Bai2 from "./pages/bai2";
-// import Bai4lab3 from "./pages/bai4"
-// import Bai3lab3  from "./pages/bai3lab3"
-// import Lab4 from "./pages/lab4";
-// import Categories from "./pages/categories";
-// import Lab5 from "./pages/lab5";
-// import Update from "./pages/update";
-// const { Header, Content, Footer } = Layout;
+import { Toaster } from "react-hot-toast";
+import { Link, Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
 
-import Header from "./components/Header";
-import { useUser } from "./context/UserContext";
+import Dashboard from "./pages/Dashboard";
+
+import Uers from "./pages/Uers";
+import Product from "./pages/Product";
+import Bai2 from "./pages/bai2";
+import Bai4lab3 from "./pages/bai4";
+import Bai3lab3 from "./pages/bai3lab3";
+import Lab4 from "./pages/lab4";
+import Categories from "./pages/categories";
+import Lab5 from "./pages/lab5";
+import Update from "./pages/update";
+import Register from "./pages/Register";
+import LOGIN from "./Login";
+import { useAuthStore } from "./stores/useAuthStore";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  const { setUser } = useUser();
-
-  const handleLogin = () => {
-    setUser({
-      name: "hoadv21",
-      avatar: "https://i.pravatar.cc/150",
-    });
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
+  const {user, logout} = useAuthStore()
   return (
-    <>
-      <Header />
+    <Layout>
+      {/* HEADER */}
+      <Header style={{ background: "#fff", padding: 0 }}>
+        <nav className="bg-blue-600 text-white shadow">
+          <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+            
+            <Link to="/" className="!text-white hover:text-gray-200">
+              <strong>WEB2091 App</strong>
+            </Link>
 
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+            <div className="hidden md:flex items-center space-x-8">
+              <Link to="/" className="!text-white">Trang chủ</Link>
+              <Link to="/dashboard" className="!text-white">Dashboard</Link>
+              <Link to="/users" className="!text-white">Users</Link>
+              <Link to="/product" className="!text-white">Product</Link>
+            </div>
+
+<div className="hidden md:flex items-center space-x-6">
+  {user ? (
+    <>
+      <span className="text-white">
+        {user.email}
+      </span>
+
+      <span className="text-green-200">
+        Đã đăng nhập
+      </span>
+ <button
+                    onClick={logout}
+                    className=""
+                  >
+                    Logout
+                  </button>
     </>
+  ) : (
+    <Link to="/login" className="!text-white">
+      Đăng nhập
+    </Link>
+  )}
+</div>
+
+          </div>
+        </nav>
+      </Header>
+
+      {/* CONTENT */}
+      <Content style={{ minHeight: "80vh" }}>
+        <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
+
+          <Routes>
+            <Route path="/" element={<h1>Trang chủ</h1>} />
+
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Uers />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/bai2" element={<Bai2 />} />
+            <Route path="/bai4lab3" element={<Bai4lab3 />} />
+            <Route path="/bai3lab3" element={<Bai3lab3 />} />
+            <Route path="/lab4" element={<Lab4 />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/lab5" element={<Lab5 />} />
+            <Route path="/update/:id" element={<Update />} />
+            <Route path="/login" element={<LOGIN />} />
+           
+            <Route path="/register" element={<Register />} /> 
+
+          </Routes>
+
+        </div>
+      </Content>
+
+      {/* FOOTER */}
+      <Footer style={{ textAlign: "center" }}>
+        WEB2091 ©2026
+      </Footer>
+
+      <Toaster />
+    </Layout>
   );
 }
-    // <Layout>
-    //   <Header style={{ background: "#fff", padding: 0 }}>
-    //     <nav className="bg-blue-600 text-white shadow">
-    //       <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
-    //         <Link to="#" className="!text-white hover:text-gray-200">
-    //           <strong>WEB2091 App</strong>
-    //         </Link>
-
-    //         <div className="hidden md:flex items-center space-x-8">
-    //           <Link to="#" className="!text-white hover:text-gray-200">
-    //             Trang chủ
-    //           </Link>
-    //             <Link to="/dashboard" className="!text-white hover:text-gray-200">
-    //             Dashboard 
-    //           </Link>
-    //           <Link to="/"className="!text-white hover:text-gray-200">
-    //             Danh sách
-    //           </Link>
-    //           <Link to="/" className="!text-white hover:text-gray-200">
-    //             Thêm mới
-    //           </Link>
-    //         </div>
-
-    //         <div className="hidden md:flex items-center space-x-6">
-    //           <Link to="#" className="!text-white hover:text-gray-200">Đăng nhập</Link>
-    //           <Link to="/login" className="!text-white hover:text-gray-200">Đăng ký</Link>
-    //         </div>
-    //       </div>
-    //     </nav>
-    //   </Header>
-
-    //   <Content style={{ minHeight: "80vh" }}>
-    //     <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
-         
-       
-    //     <Routes>
-    //     <Route path="/dashboard" element={<Dashboard/>}></Route>
-    //     <Route path="/login" element={<Login/>}></Route>
-    //     <Route path="/users" element={<Uers/>}></Route>
-    //     <Route path="/product" element={<Product/>}></Route>
-    //     <Route path="/bai2" element={<Bai2/>}></Route>
-    //     <Route path="/bai4lab3" element={<Bai4lab3/>}></Route>
-    //     <Route path="/bai3lab3" element={<Bai3lab3/>}></Route>
-    //     <Route path="/lab4" element={<Lab4/>}></Route>
-    //     <Route path="/categories" element={<Categories/>}></Route>
-    //     <Route path="/lab5" element={<Lab5/>}></Route>
-    //     <Route path="/update/:id" element={<Update/>}></Route>
-    //   </Routes>
-    //    </div>
-    //   </Content>
-
-    //   <Footer style={{ textAlign: "center" }}>
-    //     WEB2091 ©2026
-    //   </Footer>
-      
-    //   <Toaster />
-    // </Layout>
 
 export default App;
---
+++ src/Login.tsx
@@ -0,0 +1,66 @@
+import { Form, Input, Button, message } from "antd";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useAuthStore } from "./stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
+const LOGIN = () => {
+    const nvg = useNavigate()
+  const { setUser } = useAuthStore();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (values: any) => {
+      return await axios.post("http://localhost:3000/login", values);
+    },
+
+    onSuccess: ({ data }) => {
+     
+      setUser({
+        user: data.user,
+        token: data.accessToken,
+      });
+
+      message.success("Đăng nhập thành công!");
+      nvg('/')
+    },
+
+    onError: () => {
+      message.error("Sai email hoặc password!");
+    },
+  });
+
+  const onFinish = (values: any) => {
+    mutate(values);
+  };
+
+  return (
+    <Form
+      layout="vertical"
+      onFinish={onFinish}
+      style={{ maxWidth: 400, margin: "50px auto" }}
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "Nhập email!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Nhập password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" loading={isPending} block>
+          Đăng nhập
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LOGIN;
